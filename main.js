2289020d9f4d228d73d83b532c6773d89ccea302
lwatson912 <lwatson912@gmail.com>
2023-02-19 22:19:59 +0000

    Added Stopwatch

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const grid = document.getElementById('grid')
 
+// Set up the grid
+
 function setupGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
@@ -15,12 +17,97 @@ function setupGrid(size) {
     }    
 }
 
+// Create the characters to find 
+
 function wally() {
     num = Math.floor(Math.random() * 3904)
     console.log(num)
     const head = document.querySelector('#grid :nth-child(' + num + ')')
-    head.style.backgroundColor = `#FFFFFF`
+    head.style.backgroundColor = `#F6DADA`
+    const bodyOne = document.querySelector('#grid :nth-child(' + (num + 64) + ')')
+    bodyOne.style.backgroundColor = `#FF0000`
+    const bodyTwo = document.querySelector('#grid :nth-child(' + (num + 128) + ')')
+    bodyTwo.style.backgroundColor = `#FFFFFF`
+    const bodyThree = document.querySelector('#grid :nth-child(' + (num + 192) + ')')
+    bodyThree.style.backgroundColor = `#FF0000`
 }
+
+// Configure the stopwatch
+
+// Convert time to a format of hours, minutes, seconds, and milliseconds
+
+function timeToString(time) {
+    let diffInHrs = time / 3600000;
+    let hh = Math.floor(diffInHrs);
+  
+    let diffInMin = (diffInHrs - hh) * 60;
+    let mm = Math.floor(diffInMin);
+  
+    let diffInSec = (diffInMin - mm) * 60;
+    let ss = Math.floor(diffInSec);
+  
+    let diffInMs = (diffInSec - ss) * 100;
+    let ms = Math.floor(diffInMs);
+  
+    let formattedMM = mm.toString().padStart(2, "0");
+    let formattedSS = ss.toString().padStart(2, "0");
+    let formattedMS = ms.toString().padStart(2, "0");
+  
+    return `${formattedMM}:${formattedSS}:${formattedMS}`;
+  }
+  
+  // Declare variables to use in our functions below
+  
+  let startTime;
+  let elapsedTime = 0;
+  let timerInterval;
+  
+  // Create function to modify innerHTML
+  
+  function print(txt) {
+    document.getElementById("display").innerHTML = txt;
+  }
+  
+  // Create "start", "pause" and "reset" functions
+  
+  function start() {
+    startTime = Date.now() - elapsedTime;
+    timerInterval = setInterval(function printTime() {
+      elapsedTime = Date.now() - startTime;
+      print(timeToString(elapsedTime));
+    }, 10);
+    showButton("PAUSE");
+  }
+  
+  function pause() {
+    clearInterval(timerInterval);
+    // showButton("PLAY");
+  }
+  
+  function reset() {
+    clearInterval(timerInterval);
+    print("00:00:00");
+    elapsedTime = 0;
+    // showButton("PLAY");
+  }
+  
+  // Create function to display buttons
+  
+//   function showButton(buttonKey) {
+//     const buttonToShow = buttonKey === "PLAY" ? playButton : pauseButton;
+//     const buttonToHide = buttonKey === "PLAY" ? pauseButton : playButton;
+//     buttonToShow.style.display = "block";
+//     buttonToHide.style.display = "none";
+//   }
+  // Create event listeners
+  
+  let playButton = document.getElementById("start");
+  let pauseButton = document.getElementById("pause");
+  let resetButton = document.getElementById("resetButton");
+  
+  playButton.addEventListener("click", start);
+  pauseButton.addEventListener("click", pause);
+  resetButton.addEventListener("click", reset);
 
 window.onload = () => {
     setupGrid(64)
